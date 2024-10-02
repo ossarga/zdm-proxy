21f3d1381c5db25379f0e4979d278d140e771b78
Anthony Grasso <anthony.grasso@gmail.com>
2024-10-03 09:26:33 +1000

    Updated AppDynamics integration
    * Changed logic that starts a business transaction to on longer decode the frame and parse the query and instead use informatinon in statementQueryData object provided as an argument
    * Moved the location of where the start of a business transaction for a request is invoked from the clientHandler to the CQL parser to remove the need to double decode and parse the frame
    * Changed the AppDynamics start up log message level from TRACE to INFO

--- proxy/pkg/zdmproxy/cqlparser.go
+++ proxy/pkg/zdmproxy/cqlparser.go
@@ -68,6 +68,7 @@ func buildRequestInfo(
 	stmtsReplacedTerms []*statementReplacedTerms,
 	psCache *PreparedStatementCache,
 	mh *metrics.MetricHandler,
+	cc *ClientConnector,
 	currentKeyspaceName string,
 	primaryCluster common.ClusterType,
 	forwardSystemQueriesToTarget bool,
@@ -82,6 +83,7 @@ func buildRequestInfo(
 		if err != nil {
 			return nil, fmt.Errorf("could not inspect QUERY frame: %w", err)
 		}
+		cc.writeCoalescer.StartFrameBTFromQueryData(f, "ZDMProxyClientRequest", stmtQueryData)
 		return getRequestInfoFromQueryInfo(
 			frameContext.GetRawFrame(), primaryCluster,
 			forwardSystemQueriesToTarget, virtualizationEnabled, stmtQueryData.queryData), nil
@@ -107,6 +109,7 @@ func buildRequestInfo(
 		} else if len(stmtsReplacedTerms) == 1 {
 			replacedTerms = stmtsReplacedTerms[0].replacedTerms
 		}
+		cc.writeCoalescer.StartFrameBTFromQueryData(f, "ZDMProxyClientRequest", stmtQueryData)
 		return NewPrepareRequestInfo(baseRequestInfo, stmtQueryData.queryData.isFullyQualified(), replacedTerms,
 			stmtQueryData.queryData.hasPositionalBindMarkers(), currentKeyspaceName, prepareMsg.Query, prepareMsg.Keyspace), nil
 	case primitive.OpCodeBatch:
@@ -130,6 +133,7 @@ func buildRequestInfo(
 				}
 			}
 		}
+		cc.writeCoalescer.StartFrameBTFromQueryData(f, "ZDMProxyClientRequest", nil)
 		return NewBatchRequestInfo(preparedDataByStmtIdxMap), nil
 	case primitive.OpCodeExecute:
 		decodedFrame, err := frameContext.GetOrDecodeFrame()
